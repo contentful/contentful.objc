253c33e1cf5b3e636a0664022b56b9ef906be035
Boris Bügling <boris@icculus.org>
2014-03-17 17:07:44 +0100

    Support image assets in CDAFieldsViewController.

---
+++ Code/UIKit/CDAImageViewController.h
@@ -0,0 +1,17 @@
+//
+//  CDAImageViewController.h
+//  ContentfulSDK
+//
+//  Created by Boris Bügling on 17/03/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@class CDAAsset;
+
+@interface CDAImageViewController : UIViewController
+
+@property (nonatomic) CDAAsset* asset;
+
+@end
