f080e0813dfde30c8c598e6c200a8361f2ca2c5f
Boris Bügling <boris@icculus.org>
2014-03-26 13:30:51 +0100

    Updated development Pods.

--- Pods/Pods-UFO Example-environment.h
+++ Pods/Pods-UFO Example-environment.h
@@ -51,7 +51,7 @@
 // ContentfulDeliveryAPI
 #define COCOAPODS_POD_AVAILABLE_ContentfulDeliveryAPI
 #define COCOAPODS_VERSION_MAJOR_ContentfulDeliveryAPI 0
-#define COCOAPODS_VERSION_MINOR_ContentfulDeliveryAPI 1
+#define COCOAPODS_VERSION_MINOR_ContentfulDeliveryAPI 2
 #define COCOAPODS_VERSION_PATCH_ContentfulDeliveryAPI 0
 
 // ISO8601DateFormatter
