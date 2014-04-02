12ed634c3f6579a34d51b06f2e110adb911689fa
Boris Bügling <boris@icculus.org>
2014-04-02 15:02:27 +0200

    Make sure CDAClient is actually retained.

--- Examples/Catalog/CDASimpleMapViewController.m
+++ Examples/Catalog/CDASimpleMapViewController.m
@@ -10,7 +10,7 @@
 
 @interface CDASimpleMapViewController ()
 
-@property (nonatomic) CDAClient* client;
+@property (nonatomic) CDAClient* contentfulClient;
 
 @end
 
@@ -21,9 +21,11 @@
 -(id)init {
     self = [super init];
     if (self) {
-        self.client = [[CDAClient alloc] initWithSpaceKey:@"lzjz8hygvfgu" accessToken:@"0c6ef483524b5e46b3bafda1bf355f38f5f40b4830f7599f790a410860c7c271"];
-        self.coordinateFieldIdentifier = @"location";
+        self.contentfulClient = [[CDAClient alloc] initWithSpaceKey:@"lzjz8hygvfgu" accessToken:@"0c6ef483524b5e46b3bafda1bf355f38f5f40b4830f7599f790a410860c7c271"];
+        self.client = self.contentfulClient;
         self.query = @{ @"content_type": @"7ocuA1dfoccWqWwWUY4UY" };
+        
+        self.coordinateFieldIdentifier = @"location";
         self.titleFieldIdentifier = @"locationName";
     }
     return self;
