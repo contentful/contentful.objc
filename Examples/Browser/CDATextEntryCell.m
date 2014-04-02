3a491b841aed9e5fa68a65449577cadc84eb5638
Boris Bügling <boris@icculus.org>
2014-04-02 12:05:03 +0200

    Fixed vertical alignement on iOS 6.

--- Examples/Browser/CDATextEntryCell.m
+++ Examples/Browser/CDATextEntryCell.m
@@ -23,6 +23,7 @@
     self = [super initWithStyle:style reuseIdentifier:reuseIdentifier];
     if (self) {
         self.textField = [[UITextField alloc] initWithFrame:CGRectMake(0.0, 0.0, self.frame.size.width / 2, self.frame.size.height)];
+        self.textField.contentVerticalAlignment = UIControlContentVerticalAlignmentCenter;
         self.textField.clearsOnBeginEditing = YES;
         self.textField.enablesReturnKeyAutomatically = YES;
         self.textField.returnKeyType = UIReturnKeyGo;
