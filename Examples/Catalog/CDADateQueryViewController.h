5b4d427f76cd1cd94b7a7055eb383adaca46f2b8
Boris Bügling <boris@icculus.org>
2014-04-02 15:20:41 +0200

    Example of date query best practices added.

---
+++ Examples/Catalog/CDADateQueryViewController.h
@@ -0,0 +1,13 @@
+//
+//  CDADateQueryViewController.h
+//  ContentfulSDK
+//
+//  Created by Boris Bügling on 02/04/14.
+//
+//
+
+#import <ContentfulDeliveryAPI/ContentfulDeliveryAPI.h>
+
+@interface CDADateQueryViewController : CDAEntriesViewController
+
+@end
