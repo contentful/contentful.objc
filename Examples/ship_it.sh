4b69917af4e177baa01270eae7e92784a77e2b19
Boris Bügling <boris@icculus.org>
2014-04-03 10:43:04 +0200

    Exclude .DS_Store from framework ZIP as well.

--- Examples/ship_it.sh
+++ Examples/ship_it.sh
@@ -4,7 +4,7 @@ zip --symlinks -vr UFO.zip UFO/ -x "*.DS_Store"
 
 cd UFO/Distribution &&
 zip --symlinks -vr ContentfulDeliveryAPI.zip \
-  ContentfulDeliveryAPI.framework/ &&
+  ContentfulDeliveryAPI.framework/ -x "*.DS_Store" &&
 mv ContentfulDeliveryAPI.zip ../.. &&
 cd -
 
