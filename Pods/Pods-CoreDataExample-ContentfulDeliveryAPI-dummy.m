cd0cad6a615532199094fd4106b7413b30e1a8bf
Boris Bügling <boris@icculus.org>
2014-04-07 11:41:07 +0200

    Added Core Data example.

---
+++ Pods/Pods-CoreDataExample-ContentfulDeliveryAPI-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CoreDataExample_ContentfulDeliveryAPI : NSObject
+@end
+@implementation PodsDummy_Pods_CoreDataExample_ContentfulDeliveryAPI
+@end
