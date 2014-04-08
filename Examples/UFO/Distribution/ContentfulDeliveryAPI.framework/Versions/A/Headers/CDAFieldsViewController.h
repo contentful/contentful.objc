f719db8eb98ac26c7643b4e848e037d93d7217c5
Boris Bügling <boris@icculus.org>
2014-04-08 17:10:58 +0200

    Update static framework build.

--- Examples/UFO/Distribution/ContentfulDeliveryAPI.framework/Versions/A/Headers/CDAFieldsViewController.h
+++ Examples/UFO/Distribution/ContentfulDeliveryAPI.framework/Versions/A/Headers/CDAFieldsViewController.h
@@ -19,6 +19,16 @@
  */
 @interface CDAFieldsViewController : UITableViewController
 
+/**
+ *  By default, values for all Fields of the Entry will be shown. If you want to limit which Fields
+ *  are shown and also influence the order in which they are shown, override this method in your
+ *  subclass.
+ *
+ *  If this method returns `nil`, all Fields will be shown in alphabetical order.
+ *
+ *  @return An array of Field identifiers as strings. Any identifiers which do not match actual
+ *      Fields on the Entry will be ignored.
+ */
 @property (nonatomic, readonly) NSArray* visibleFields;
 
 /** @name Initializing the CDAEntriesViewController Object */
@@ -60,17 +70,5 @@
  *  @param error The error which occured.
  */
 -(void)showError:(NSError*)error;
-
-/**
- *  By default, values for all Fields of the Entry will be shown. If you want to limit which Fields
- *  are shown and also influence the order in which they are shown, override this method in your
- *  subclass.
- *
- *  If this method returns `nil`, all Fields will be shown in alphabetical order.
- *
- *  @return An array of Field identifiers as strings. Any identifiers which do not match actual
- *      Fields on the Entry will be ignored.
- */
--(NSArray*)visibleFields;
 
 @end
