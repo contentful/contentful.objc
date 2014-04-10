5378e657e842105e0a55fce723a8e10bb74b6121
Boris Bügling <boris@icculus.org>
2014-04-10 11:00:54 +0200

    Refactored resolving of links, so that an Entry can also be queried for still unresolved Resources.

---
+++ Code/CDAEntry+Private.h
@@ -0,0 +1,16 @@
+//
+//  CDAEntry+Private.h
+//  ContentfulSDK
+//
+//  Created by Boris Bügling on 04/03/14.
+//
+//
+
+#import "CDAEntry.h"
+
+@interface CDAEntry ()
+
+-(NSArray*)findUnresolvedAssets;
+-(NSArray*)findUnresolvedEntries;
+
+@end
