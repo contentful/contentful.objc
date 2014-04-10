d7462f0cb401e5690bdbc6ea13a7d555bdffec83
Boris Bügling <boris@icculus.org>
2014-04-10 15:53:36 +0200

    Demonstrate simple caching in Asset loading example.

--- Examples/Catalog/CDALoadAssetsViewController.m
+++ Examples/Catalog/CDALoadAssetsViewController.m
@@ -29,14 +29,23 @@
     UIImageView* imageView = [[UIImageView alloc] initWithFrame:self.view.bounds];
     imageView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
     imageView.contentMode = UIViewContentModeScaleAspectFit;
+    imageView.offlineCaching_cda = YES;
     [self.view addSubview:imageView];
+    
+     NSString* cacheFilePath = [[NSSearchPathForDirectoriesInDomains(NSCachesDirectory, NSUserDomainMask, YES) firstObject] stringByAppendingPathComponent:@"asset.data"];
     
     self.client = [CDAClient new];
     [self.client fetchEntryWithIdentifier:@"nyancat"
                                   success:^(CDAResponse *response, CDAEntry *entry) {
-                                      [imageView cda_setImageWithAsset:entry.fields[@"image"]];
+                                      CDAAsset* asset = entry.fields[@"image"];
+                                      [imageView cda_setImageWithAsset:asset];
+                                      [asset writeToFile:cacheFilePath];
                                   }
-                                  failure:nil];
+                                  failure:^(CDAResponse *response, NSError *error) {
+                                      CDAAsset* asset = [CDAAsset readFromFile:cacheFilePath
+                                                                        client:self.client];
+                                      [imageView cda_setImageWithAsset:asset];
+                                  }];
 }
 
 @end
