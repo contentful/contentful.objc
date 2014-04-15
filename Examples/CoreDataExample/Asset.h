e14afb17e2f3c5bf32a07dad2d75a77b4d89e574
Boris Bügling <boris@icculus.org>
2014-04-15 14:47:59 +0200

    Added a test case for Core Data.

---
+++ Examples/CoreDataExample/Asset.h
@@ -0,0 +1,20 @@
+//
+//  Asset.h
+//  ContentfulSDK
+//
+//  Created by Boris Bügling on 14/04/14.
+//
+//
+
+#import <ContentfulDeliveryAPI/CDAPersistedAsset.h>
+#import <CoreData/CoreData.h>
+#import <Foundation/Foundation.h>
+
+@interface Asset : NSManagedObject <CDAPersistedAsset>
+
+@property (nonatomic, retain) NSString * identifier;
+@property (nonatomic, retain) NSString * url;
+@property (nonatomic, retain) NSString * mimeType;
+@property (nonatomic, retain) NSManagedObject *cat;
+
+@end
---
+++ Examples/CoreDataExample/ManagedCat.h
@@ -0,0 +1,23 @@
+//
+//  ManagedCat.h
+//  ContentfulSDK
+//
+//  Created by Boris Bügling on 14/04/14.
+//
+//
+
+#import <ContentfulDeliveryAPI/CDAPersistedEntry.h>
+#import <CoreData/CoreData.h>
+#import <Foundation/Foundation.h>
+
+@class Asset;
+
+@interface ManagedCat : NSManagedObject <CDAPersistedEntry>
+
+@property (nonatomic, retain) NSString * name;
+@property (nonatomic, retain) NSString * color;
+@property (nonatomic, retain) NSNumber * livesLeft;
+@property (nonatomic, retain) NSString * identifier;
+@property (nonatomic, retain) Asset *picture;
+
+@end
