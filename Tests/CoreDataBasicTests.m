74fec825451c35afc85b84c2b6f2bc3742dada42
Boris Bügling <boris@icculus.org>
2014-04-16 11:43:43 +0200

    Update tests for Core Data persistence.

--- Tests/CoreDataBasicTests.m
+++ Tests/CoreDataBasicTests.m
@@ -15,6 +15,7 @@
 @interface CoreDataBasicTests : SyncBaseTestCase
 
 @property (nonatomic) CoreDataManager* coreDataManager;
+@property (nonatomic) NSDate* lastSyncTimestamp;
 
 @end
 
@@ -22,9 +23,16 @@
 
 @implementation CoreDataBasicTests
 
--(void)setUp {
-    [super setUp];
+-(void)assertNumberOfAssets:(NSUInteger)numberOfAssets numberOfEntries:(NSUInteger)numberOfEntries {
+    XCTAssertEqual(numberOfAssets, [self.coreDataManager fetchAssetsFromDataStore].count, @"");
+    XCTAssertEqual(numberOfEntries, [self.coreDataManager fetchEntriesFromDataStore].count, @"");
     
+    NSDate* timestamp = [self.coreDataManager fetchSpaceFromDataStore].lastSyncTimestamp;
+    XCTAssertNotEqualObjects(self.lastSyncTimestamp, timestamp, @"");
+    self.lastSyncTimestamp = timestamp;
+}
+
+-(void)buildCoreDataManager {
     self.coreDataManager = [[CoreDataManager alloc] initWithClient:self.client
                                                      dataModelName:@"CoreDataExample"];
     self.coreDataManager.classForAssets = [Asset class];
@@ -34,7 +42,28 @@
     self.coreDataManager.mappingForEntries = @{ @"contentType.identifier": @"contentTypeIdentifier",
                                                 @"fields.color": @"color",
                                                 @"fields.livesLeft": @"livesLeft",
-                                                @"fields.name": @"name" };
+                                                @"fields.name": @"name",
+                                                @"fields.title": @"name" };
+}
+
+-(void)setUp {
+    [super setUp];
+    
+    self.lastSyncTimestamp = nil;
+    
+    /*
+     Map URLs to JSON response files
+     
+     The tests are based on a sync session with five subsequent syncs where each one either added,
+     removed or updated Resources.
+     */
+    NSDictionary* stubs = @{ @"https://cdn.contentful.com/spaces/emh6o2ireilu/sync?access_token=1bf1261e0225089be464c79fff1a0773ca8214f1e82dd521f3ecf9690ba888ac&initial=true": @"initial", @"https://cdn.contentful.com/spaces/emh6o2ireilu/sync?access_token=1bf1261e0225089be464c79fff1a0773ca8214f1e82dd521f3ecf9690ba888ac&sync_token=w5ZGw6JFwqZmVcKsE8Kow4grw45QdyYxwoHDtsKywrXDmQs_WcOvIcOzwotYw6PCgcOsAcOYYcO4YsKCw7TCnsK_clnClS7Csx9lwoFcw6nCqnnCpWh3w7k7SkI-CcOuQyXDlw_Dlh9RwqkcElwpW30sw4k": @"added", @"https://cdn.contentful.com/spaces/emh6o2ireilu/sync?access_token=1bf1261e0225089be464c79fff1a0773ca8214f1e82dd521f3ecf9690ba888ac&sync_token=w5ZGw6JFwqZmVcKsE8Kow4grw45QdyY0w4bCiMKOWDIFw61bwqQ_w73CnMKsB8KpwrFZPsOZw5ZQwqDDnUA0w5tOPRtwwoAkwpJMTzghdEnDjCkiw5fCuynDlsO5DyvCsjgQa2TDisKNZ8Kqw4TCjhZIGQ": @"deleted", @"https://cdn.contentful.com/spaces/emh6o2ireilu/sync?access_token=1bf1261e0225089be464c79fff1a0773ca8214f1e82dd521f3ecf9690ba888ac&sync_token=w5ZGw6JFwqZmVcKsE8Kow4grw45QdyZew5xDN04dJg3DkmBAw4XDh8OEw5o5UVhIw6nDlFjDoBxIasKIDsKIw4VcIV18GicdwoTDjCtoMiFAfcKiwrRKIsKYwrzCmMKBw4ZhwrdhwrsGa8KTwpQ6w6A": @"added-asset", @"https://cdn.contentful.com/spaces/emh6o2ireilu/sync?access_token=1bf1261e0225089be464c79fff1a0773ca8214f1e82dd521f3ecf9690ba888ac&sync_token=w5ZGw6JFwqZmVcKsE8Kow4grw45QdyZ_NnHDoQzCtcKoMh9KZHtAWcObw7XCimZgVGPChUfDuxQHwoHDosO6CcKodsO2MWJQwrrCrsOswpl5w6LCuV0tw4Njwo9Ww5fCl8KqEgB6XgAJNVF2wpk3Lg": @"deleted-asset", @"https://cdn.contentful.com/spaces/emh6o2ireilu/sync?access_token=1bf1261e0225089be464c79fff1a0773ca8214f1e82dd521f3ecf9690ba888ac&sync_token=w5ZGw6JFwqZmVcKsE8Kow4grw45QdyYHPUPDhggxwr5qw5RBbMKWw4VjOg3DumTDg0_CgsKcYsO8UcOZfMKLw4sKUcOnJcKxfDUkGWwxNMOVw4AiacK5Bmo4ScOhI0g2cXLClxTClsOyE8OOc8O3": @"update", @"https://cdn.contentful.com/spaces/emh6o2ireilu/?access_token=1bf1261e0225089be464c79fff1a0773ca8214f1e82dd521f3ecf9690ba888ac": @"space", };
+    
+    [self stubHTTPRequestUsingFixtures:stubs inDirectory:@"SyncTests"];
+    
+    [self buildCoreDataManager];
+    [[NSFileManager defaultManager] removeItemAtURL:self.coreDataManager.storeURL
+                                              error:nil];
 }
 
 -(void)tearDown {
@@ -46,12 +75,85 @@
 
 #pragma mark -
 
+-(void)testContinueSyncFromDataStore {
+    StartBlock();
+    
+    [self.coreDataManager performSynchronizationWithSuccess:^{
+        [self assertNumberOfAssets:1U numberOfEntries:1U];
+        [self buildCoreDataManager];
+        
+        Asset* asset = [[self.coreDataManager fetchAssetsFromDataStore] firstObject];
+        XCTAssertEqualObjects(@"512_black.png", asset.url.lastPathComponent, @"");
+        ManagedCat* cat = [[self.coreDataManager fetchEntriesFromDataStore] firstObject];
+        XCTAssertEqualObjects(@"Test", cat.name, @"");
+        
+        [self.coreDataManager performSynchronizationWithSuccess:^{
+            [self assertNumberOfAssets:1U numberOfEntries:2U];
+            [self buildCoreDataManager];
+            
+            [self.coreDataManager performSynchronizationWithSuccess:^{
+                [self assertNumberOfAssets:1U numberOfEntries:1U];
+                [self buildCoreDataManager];
+                
+                [self.coreDataManager performSynchronizationWithSuccess:^{
+                    [self assertNumberOfAssets:2U numberOfEntries:1U];
+                    [self buildCoreDataManager];
+                    
+                    [self.coreDataManager performSynchronizationWithSuccess:^{
+                        [self assertNumberOfAssets:1U numberOfEntries:1U];
+                        [self buildCoreDataManager];
+                        
+                        [self.coreDataManager performSynchronizationWithSuccess:^{
+                            [self assertNumberOfAssets:1U numberOfEntries:1U];
+                            
+                            Asset* asset = [[self.coreDataManager fetchAssetsFromDataStore] firstObject];
+                            XCTAssertEqualObjects(@"vaa4by0.png", asset.url.lastPathComponent, @"");
+                            ManagedCat* cat = [[self.coreDataManager fetchEntriesFromDataStore] firstObject];
+                            XCTAssertEqualObjects(@"Test (changed)", cat.name, @"");
+                            
+                            EndBlock();
+                        } failure:^(CDAResponse *response, NSError *error) {
+                            XCTFail(@"Error: %@", error);
+                            
+                            EndBlock();
+                        }];
+                    } failure:^(CDAResponse *response, NSError *error) {
+                        XCTFail(@"Error: %@", error);
+                        
+                        EndBlock();
+                    }];
+                    
+                } failure:^(CDAResponse *response, NSError *error) {
+                    XCTFail(@"Error: %@", error);
+                    
+                    EndBlock();
+                }];
+                
+            } failure:^(CDAResponse *response, NSError *error) {
+                XCTFail(@"Error: %@", error);
+                
+                EndBlock();
+            }];
+        } failure:^(CDAResponse *response, NSError *error) {
+            XCTFail(@"Error: %@", error);
+            
+            EndBlock();
+        }];
+    } failure:^(CDAResponse *response, NSError *error) {
+        XCTFail(@"Error: %@", error);
+        
+        EndBlock();
+    }];
+    
+    WaitUntilBlockCompletes();
+}
+
 -(void)testInitialSync {
     StartBlock();
     
     [self.coreDataManager performSynchronizationWithSuccess:^{
-        XCTAssertEqual(2U, [self.coreDataManager fetchAssetsFromDataStore].count, @"");
-        XCTAssertEqual(7U, [self.coreDataManager fetchEntriesFromDataStore].count, @"");
+        XCTAssertEqual(1U, [self.coreDataManager fetchAssetsFromDataStore].count, @"");
+        XCTAssertEqual(1U, [self.coreDataManager fetchEntriesFromDataStore].count, @"");
         
         EndBlock();
     } failure:^(CDAResponse *response, NSError *error) {
