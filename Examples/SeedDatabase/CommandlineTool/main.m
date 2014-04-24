095161c4bc01d67dcba80cbeb7b843f9a1a78fb9
Boris Bügling <boris@icculus.org>
2014-04-24 17:58:45 +0200

    Added data model for seed database example.

--- Examples/SeedDatabase/CommandlineTool/main.m
+++ Examples/SeedDatabase/CommandlineTool/main.m
@@ -6,17 +6,46 @@
 //
 //
 
-#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
+#import <ContentfulDeliveryAPI/ContentfulDeliveryAPI.h>
 
-int main(int argc, const char * argv[])
-{
+#import "Asset.h"
+#import "CoreDataManager.h"
+#import "Document.h"
+#import "SyncInfo.h"
 
+static NSString* const CDAAccessToken   = @"a196a5806ddd5f25700624bb11dfc94aeac9f0a5d4bd245e68cf42f78f8b2cc6";
+static NSString* const CDASpaceKey      = @"duzidfp33ikw";
+
+int main(int argc, const char * argv[]) {
     @autoreleasepool {
+        NSApplicationLoad();
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        CDAClient* client = [[CDAClient alloc] initWithSpaceKey:CDASpaceKey accessToken:CDAAccessToken];
+        CoreDataManager* manager = [[CoreDataManager alloc] initWithClient:client dataModelName:@"Doge"];
         
+        manager.classForAssets = [Asset class];
+        manager.classForEntries = [Document class];
+        manager.classForSpaces = [SyncInfo class];
+        
+        manager.mappingForEntries = @{ @"fields.abstract": @"abstract",
+                                       @"fields.title": @"title",
+                                       @"fields.document": @"document",
+                                       @"fields.thumbnail": @"thumbnail" };
+        
+        [manager performSynchronizationWithSuccess:^{
+            NSLog(@"Successfully generated your seed database.");
+            
+            [NSApp terminate:nil];
+        } failure:^(CDAResponse *response, NSError *error) {
+            NSLog(@"Error: %@", error);
+            
+            [NSApp terminate:nil];
+        }];
+        
+        [NSApp run];
     }
+    
     return 0;
 }
 
