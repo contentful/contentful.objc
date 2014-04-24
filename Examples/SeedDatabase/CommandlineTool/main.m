544743b06fe5612819ab3065210dbb38acfe7809
Boris Bügling <boris@icculus.org>
2014-04-24 17:30:37 +0200

    Added new target for seed database command line tool example.

---
+++ Examples/SeedDatabase/CommandlineTool/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ContentfulSeedDatabase
+//
+//  Created by Boris Bügling on 24/04/14.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
---
+++ Pods/Pods-ContentfulSeedDatabase-HRCoder-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_ContentfulSeedDatabase_HRCoder : NSObject
+@end
+@implementation PodsDummy_Pods_ContentfulSeedDatabase_HRCoder
+@end
