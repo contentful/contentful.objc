e48e4fbf723a2d4a5dd6612d121bc3a558a89789
Boris Bügling <boris@icculus.org>
2014-04-25 10:41:29 +0200

    Added sample app for seed database example.

---
+++ Examples/SeedDatabase/CDAAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  CDAAppDelegate.h
+//  SeedDatabaseExample
+//
+//  Created by Boris Bügling on 25/04/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CDAAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
