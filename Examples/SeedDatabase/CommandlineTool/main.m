680c6a516e225f8d8dfde62f40a19db253409069
Boris Bügling <boris@icculus.org>
2014-04-25 10:29:08 +0200

    Seed database tool fetches all relevant data now.

--- Examples/SeedDatabase/CommandlineTool/main.m
+++ Examples/SeedDatabase/CommandlineTool/main.m
@@ -14,6 +14,16 @@
 #import "Document.h"
 #import "SyncInfo.h"
 
+extern NSString* CDACacheDirectory();
+
+/*
+ Change this to match the Space you want to generate a seed database for. You also need to modify the
+ code to use the correct data model, managed object classes and mapping for Entries.
+ 
+ When running the commandline tool, it will pre-populate an SQLite store with data and also fetch
+ all available Assets to flat files. Those can be included in your bundle to make the app work offline
+ from the start.
+ */
 static NSString* const CDAAccessToken   = @"a196a5806ddd5f25700624bb11dfc94aeac9f0a5d4bd245e68cf42f78f8b2cc6";
 static NSString* const CDASpaceKey      = @"duzidfp33ikw";
 
@@ -34,7 +44,18 @@ int main(int argc, const char * argv[]) {
                                        @"fields.thumbnail": @"thumbnail" };
         
         [manager performSynchronizationWithSuccess:^{
-            NSLog(@"Successfully generated your seed database.");
+            for (id<CDAPersistedAsset> asset in [manager fetchAssetsFromDataStore]) {
+                NSLog(@"Fetching asset from %@", asset.url);
+                NSData* data = [NSData dataWithContentsOfURL:[NSURL URLWithString:asset.url]];
+                
+                NSString* fileName = [NSString stringWithFormat:@"cache_%@_Asset_%@.data",
+                                      CDASpaceKey, asset.identifier];
+                fileName = [CDACacheDirectory() stringByAppendingPathComponent:fileName];
+                [data writeToFile:fileName atomically:YES];
+            }
+            
+            NSLog(@"Successfully generated your seed database at '%@'", manager.storeURL.path);
+            NSLog(@"Assets are stored at '%@'", CDACacheDirectory());
             
             [NSApp terminate:nil];
         } failure:^(CDAResponse *response, NSError *error) {
