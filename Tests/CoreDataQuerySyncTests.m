8512f6f536e362cb702a29c6da450dc9a3f221c3
Boris Bügling <boris@icculus.org>
2014-04-28 09:32:39 +0200

    Extracted Core Data testing stuff into a base test case.

---
+++ Tests/CoreDataQuerySyncTests.m
@@ -0,0 +1,17 @@
+//
+//  CoreDataQuerySyncTests.m
+//  ContentfulSDK
+//
+//  Created by Boris Bügling on 28/04/14.
+//
+//
+
+#import "CoreDataBaseTestCase.h"
+
+@interface CoreDataQuerySyncTests : CoreDataBaseTestCase
+
+@end
+
+@implementation CoreDataQuerySyncTests
+
+@end
