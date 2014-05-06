a2afbca8ffa7e63ae07dd763ef2118ccc7c53c8f
Boris Bügling <boris@icculus.org>
2014-05-06 16:43:02 +0200

    New Content Types view controller which uses preview view controller instead of the plain fields view controller.

---
+++ Examples/Browser/CDAContentTypesViewController.m
@@ -0,0 +1,53 @@
+//
+//  CDAContentTypesViewController.m
+//  ContentfulSDK
+//
+//  Created by Boris Bügling on 05/05/14.
+//
+//
+
+#import "CDAContentTypesViewController.h"
+#import "CDAEntryPreviewController.h"
+
+@interface CDAContentTypesViewController () <CDAEntriesViewControllerDelegate>
+
+@end
+
+#pragma mark -
+
+@implementation CDAContentTypesViewController
+
+-(void)didSelectRowWithResource:(CDAResource *)resource {
+    CDAContentType* contentType = (CDAContentType*)resource;
+    
+    NSDictionary* mapping = nil;
+    if (contentType.displayField) {
+        mapping = @{ @"textLabel.text": [@"fields." stringByAppendingString:contentType.displayField] };
+    }
+    
+    CDAEntriesViewController* entriesVC = [[CDAEntriesViewController alloc] initWithCellMapping:mapping];
+    entriesVC.client = self.client;
+    entriesVC.delegate = self;
+    entriesVC.query = @{ @"content_type": contentType.identifier };
+    
+    [self.navigationController pushViewController:entriesVC animated:YES];
+}
+
+-(id)init {
+    self = [super initWithCellMapping:@{ @"textLabel.text": @"name" }];
+    if (self) {
+        self.resourceType = CDAResourceTypeContentType;
+        self.title = NSLocalizedString(@"Preview", nil);
+    }
+    return self;
+}
+
+#pragma mark - CDAEntriesViewControllerDelegate
+
+-(void)entriesViewController:(CDAEntriesViewController *)entriesViewController
+       didSelectRowWithEntry:(CDAEntry *)entry {
+    CDAEntryPreviewController* previewController = [[CDAEntryPreviewController alloc] initWithEntry:entry];
+    [self.navigationController pushViewController:previewController animated:YES];
+}
+
+@end
