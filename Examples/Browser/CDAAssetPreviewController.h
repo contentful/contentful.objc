5875014547e8bb5f1ee0c9a2a69560d66edc87f1
Boris Bügling <boris@icculus.org>
2014-05-06 13:41:17 +0200

    QuickLook previews for Assets.

---
+++ Examples/Browser/CDAAssetPreviewController.h
@@ -0,0 +1,28 @@
+//
+//  CDAAssetPreviewController.h
+//  ContentfulSDK
+//
+//  Created by Boris Bügling on 05/05/14.
+//
+//
+
+#import <QuickLook/QuickLook.h>
+
+@class CDAAsset;
+@class CDAAssetPreviewController;
+
+@protocol CDAAssetPreviewControllerDelegate <NSObject>
+
+-(void)assetPreviewControllerDidLoadAssetPreview:(CDAAssetPreviewController*)assetPreviewController;
+
+@end
+
+#pragma mark -
+
+@interface CDAAssetPreviewController : QLPreviewController
+
+@property (nonatomic, weak) id<CDAAssetPreviewControllerDelegate> previewDelegate;
+
+-(id)initWithAsset:(CDAAsset*)asset;
+
+@end
