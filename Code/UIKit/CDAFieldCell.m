090d8d9d20f252136e2598ba17e0fbf3df25d87d
Boris Bügling <boris@icculus.org>
2014-05-07 14:15:15 +0200

    Change style for cells in CDAFieldViewController.

--- Code/UIKit/CDAFieldCell.m
+++ Code/UIKit/CDAFieldCell.m
@@ -15,7 +15,7 @@
 @implementation CDAFieldCell
 
 -(id)initWithStyle:(UITableViewCellStyle)style reuseIdentifier:(NSString *)reuseIdentifier {
-    self = [super initWithStyle:UITableViewCellStyleValue2 reuseIdentifier:reuseIdentifier];
+    self = [super initWithStyle:UITableViewCellStyleValue1 reuseIdentifier:reuseIdentifier];
     return self;
 }
 
