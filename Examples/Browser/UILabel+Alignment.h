cb72293c3d983e08dc28558f051649bfc1efd5cf
Boris Bügling <boris@icculus.org>
2014-06-11 03:44:46 +0200

    Improved about view controller.

---
+++ Examples/Browser/UILabel+Alignment.h
@@ -0,0 +1,15 @@
+//
+//  UILabel+Alignment.h
+//  ContentfulSDK
+//
+//  Created by Boris Bügling on 21/05/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UILabel (Alignment)
+
+- (void)cda_alignBottom;
+
+@end
