b7507ef694e02a446e0f351bd15aa5196c5906a4
Boris Bügling <boris@icculus.org>
2014-06-11 04:10:42 +0200

    Basic implementation of the tutorial.

---
+++ Examples/Browser/CDATutorialView.h
@@ -0,0 +1,18 @@
+//
+//  CDATutorialView.h
+//  ContentfulSDK
+//
+//  Created by Boris Bügling on 22/05/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CDATutorialView : UIView
+
+@property (nonatomic, readonly) UIImageView* backgroundImageView;
+@property (nonatomic, readonly) UILabel* body;
+@property (nonatomic, readonly) UILabel* headline;
+@property (nonatomic, readonly) UIImageView* imageView;
+
+@end
