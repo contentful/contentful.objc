a2be642f30040848c75bd34b87a74a6818b23ebd
Boris Bügling <boris@icculus.org>
2014-06-17 14:37:40 +0200

    Reliably reproduced the issue from support issue #932.
    The issue is republishing an entry which was unpublished earlier. It will not be reinserted into the database.

--- Tests/CoreDataBasicTests.m
+++ Tests/CoreDataBasicTests.m
@@ -25,7 +25,7 @@
      The tests are based on a sync session with five subsequent syncs where each one either added,
      removed or updated Resources.
      */
-    NSDictionary* stubs = @{ @"https://cdn.contentful.com/spaces/emh6o2ireilu/sync?access_token=1bf1261e0225089be464c79fff1a0773ca8214f1e82dd521f3ecf9690ba888ac&initial=true": @"initial", @"https://cdn.contentful.com/spaces/emh6o2ireilu/sync?access_token=1bf1261e0225089be464c79fff1a0773ca8214f1e82dd521f3ecf9690ba888ac&sync_token=w5ZGw6JFwqZmVcKsE8Kow4grw45QdyYxwoHDtsKywrXDmQs_WcOvIcOzwotYw6PCgcOsAcOYYcO4YsKCw7TCnsK_clnClS7Csx9lwoFcw6nCqnnCpWh3w7k7SkI-CcOuQyXDlw_Dlh9RwqkcElwpW30sw4k": @"added", @"https://cdn.contentful.com/spaces/emh6o2ireilu/sync?access_token=1bf1261e0225089be464c79fff1a0773ca8214f1e82dd521f3ecf9690ba888ac&sync_token=w5ZGw6JFwqZmVcKsE8Kow4grw45QdyY0w4bCiMKOWDIFw61bwqQ_w73CnMKsB8KpwrFZPsOZw5ZQwqDDnUA0w5tOPRtwwoAkwpJMTzghdEnDjCkiw5fCuynDlsO5DyvCsjgQa2TDisKNZ8Kqw4TCjhZIGQ": @"deleted", @"https://cdn.contentful.com/spaces/emh6o2ireilu/sync?access_token=1bf1261e0225089be464c79fff1a0773ca8214f1e82dd521f3ecf9690ba888ac&sync_token=w5ZGw6JFwqZmVcKsE8Kow4grw45QdyZew5xDN04dJg3DkmBAw4XDh8OEw5o5UVhIw6nDlFjDoBxIasKIDsKIw4VcIV18GicdwoTDjCtoMiFAfcKiwrRKIsKYwrzCmMKBw4ZhwrdhwrsGa8KTwpQ6w6A": @"added-asset", @"https://cdn.contentful.com/spaces/emh6o2ireilu/sync?access_token=1bf1261e0225089be464c79fff1a0773ca8214f1e82dd521f3ecf9690ba888ac&sync_token=w5ZGw6JFwqZmVcKsE8Kow4grw45QdyZ_NnHDoQzCtcKoMh9KZHtAWcObw7XCimZgVGPChUfDuxQHwoHDosO6CcKodsO2MWJQwrrCrsOswpl5w6LCuV0tw4Njwo9Ww5fCl8KqEgB6XgAJNVF2wpk3Lg": @"deleted-asset", @"https://cdn.contentful.com/spaces/emh6o2ireilu/sync?access_token=1bf1261e0225089be464c79fff1a0773ca8214f1e82dd521f3ecf9690ba888ac&sync_token=w5ZGw6JFwqZmVcKsE8Kow4grw45QdyYHPUPDhggxwr5qw5RBbMKWw4VjOg3DumTDg0_CgsKcYsO8UcOZfMKLw4sKUcOnJcKxfDUkGWwxNMOVw4AiacK5Bmo4ScOhI0g2cXLClxTClsOyE8OOc8O3": @"update", @"https://cdn.contentful.com/spaces/emh6o2ireilu/?access_token=1bf1261e0225089be464c79fff1a0773ca8214f1e82dd521f3ecf9690ba888ac": @"space", };
+    NSDictionary* stubs = @{ @"https://cdn.contentful.com/spaces/emh6o2ireilu/sync?access_token=1bf1261e0225089be464c79fff1a0773ca8214f1e82dd521f3ecf9690ba888ac&initial=true": @"initial", @"https://cdn.contentful.com/spaces/emh6o2ireilu/sync?access_token=1bf1261e0225089be464c79fff1a0773ca8214f1e82dd521f3ecf9690ba888ac&sync_token=w5ZGw6JFwqZmVcKsE8Kow4grw45QdyYxwoHDtsKywrXDmQs_WcOvIcOzwotYw6PCgcOsAcOYYcO4YsKCw7TCnsK_clnClS7Csx9lwoFcw6nCqnnCpWh3w7k7SkI-CcOuQyXDlw_Dlh9RwqkcElwpW30sw4k": @"added", @"https://cdn.contentful.com/spaces/emh6o2ireilu/sync?access_token=1bf1261e0225089be464c79fff1a0773ca8214f1e82dd521f3ecf9690ba888ac&sync_token=w5ZGw6JFwqZmVcKsE8Kow4grw45QdyY0w4bCiMKOWDIFw61bwqQ_w73CnMKsB8KpwrFZPsOZw5ZQwqDDnUA0w5tOPRtwwoAkwpJMTzghdEnDjCkiw5fCuynDlsO5DyvCsjgQa2TDisKNZ8Kqw4TCjhZIGQ": @"deleted", @"https://cdn.contentful.com/spaces/emh6o2ireilu/sync?access_token=1bf1261e0225089be464c79fff1a0773ca8214f1e82dd521f3ecf9690ba888ac&sync_token=w5ZGw6JFwqZmVcKsE8Kow4grw45QdyZew5xDN04dJg3DkmBAw4XDh8OEw5o5UVhIw6nDlFjDoBxIasKIDsKIw4VcIV18GicdwoTDjCtoMiFAfcKiwrRKIsKYwrzCmMKBw4ZhwrdhwrsGa8KTwpQ6w6A": @"added-asset", @"https://cdn.contentful.com/spaces/emh6o2ireilu/sync?access_token=1bf1261e0225089be464c79fff1a0773ca8214f1e82dd521f3ecf9690ba888ac&sync_token=w5ZGw6JFwqZmVcKsE8Kow4grw45QdyZ_NnHDoQzCtcKoMh9KZHtAWcObw7XCimZgVGPChUfDuxQHwoHDosO6CcKodsO2MWJQwrrCrsOswpl5w6LCuV0tw4Njwo9Ww5fCl8KqEgB6XgAJNVF2wpk3Lg": @"deleted-asset", @"https://cdn.contentful.com/spaces/emh6o2ireilu/sync?access_token=1bf1261e0225089be464c79fff1a0773ca8214f1e82dd521f3ecf9690ba888ac&sync_token=w5ZGw6JFwqZmVcKsE8Kow4grw45QdyYHPUPDhggxwr5qw5RBbMKWw4VjOg3DumTDg0_CgsKcYsO8UcOZfMKLw4sKUcOnJcKxfDUkGWwxNMOVw4AiacK5Bmo4ScOhI0g2cXLClxTClsOyE8OOc8O3": @"update", @"https://cdn.contentful.com/spaces/emh6o2ireilu/?access_token=1bf1261e0225089be464c79fff1a0773ca8214f1e82dd521f3ecf9690ba888ac": @"space", @"https://cdn.contentful.com/spaces/giq5kda3eap4/sync?access_token=9823965a99805cf9bd6f091b2faf6eef652eff12ae0c79acacd370f873bc6fe0&initial=true": @"932-initial", @"https://cdn.contentful.com/spaces/giq5kda3eap4/?access_token=9823965a99805cf9bd6f091b2faf6eef652eff12ae0c79acacd370f873bc6fe0": @"932-space", @"https://cdn.contentful.com/spaces/giq5kda3eap4/content_types?access_token=9823965a99805cf9bd6f091b2faf6eef652eff12ae0c79acacd370f873bc6fe0&limit=1&sys.id%5Bin%5D=1nGOrvlRTaMcyyq4IEa8ea": @"932-content-types", @"https://cdn.contentful.com/spaces/giq5kda3eap4/sync?access_token=9823965a99805cf9bd6f091b2faf6eef652eff12ae0c79acacd370f873bc6fe0&sync_token=w5ZGw6JFwqZmVcKsE8Kow4grw45QdybDqiDCvSIrAFoiw7XDkQrCu8K7N8OkwqlhwoHCscOLM8Knwr7Cp8Ogw7LCjE3DicO8w6cCwoFwO1AVw6B8DGTCtlbDgMKDw7zDicKawrXDqsKkSX7DnMO-ThjDtMORwqHDhk_Ct1o": @"932-add-entry", @"https://cdn.contentful.com/spaces/giq5kda3eap4/sync?access_token=9823965a99805cf9bd6f091b2faf6eef652eff12ae0c79acacd370f873bc6fe0&sync_token=w5ZGw6JFwqZmVcKsE8Kow4grw45QdyY5dsK6w79SE2ADw41bNA58M8KLw5ZzWcKMw5Q-w4Nxw6UzCSTCtQPCrcK5w63CmcKtwq9IwqM6V8OrCsKow6LCisO9K8KfSiEKw5PCnVZQBQfCrsOiwoweCcK0wpvDoQ": @"932-unpublish-entry", @"https://cdn.contentful.com/spaces/giq5kda3eap4/sync?access_token=9823965a99805cf9bd6f091b2faf6eef652eff12ae0c79acacd370f873bc6fe0&sync_token=w5ZGw6JFwqZmVcKsE8Kow4grw45QdybCqx_CocOlwpNuw41raMKrw7nCv8OAwrnCihwDwqnCmUbCjhnDmTojwrZAw7vDuMKCH8Kcw6USTMOqw4NAwo7Cn8KUaVYXwrHCkVLCmsOgNxs-wptWw5LDl8KIw4vCvsOTwq7CsnJJ": @"932-republish-entry" };
     
     [self stubHTTPRequestUsingFixtures:stubs inDirectory:@"SyncTests"];
 }
@@ -235,6 +235,53 @@
         XCTAssertNotNil(nyanCat.picture.url, @"");
         
         EndBlock();
+    } failure:^(CDAResponse *response, NSError *error) {
+        XCTFail(@"Error: %@", error);
+        
+        EndBlock();
+    }];
+    
+    WaitUntilBlockCompletes();
+}
+
+-(void)testSyncWithRepublishedEntry {
+    self.client = [[CDAClient alloc] initWithSpaceKey:@"giq5kda3eap4" accessToken:@"9823965a99805cf9bd6f091b2faf6eef652eff12ae0c79acacd370f873bc6fe0"];
+    [self buildCoreDataManagerWithDefaultClient:NO];
+    
+    StartBlock();
+    
+    [self.coreDataManager performSynchronizationWithSuccess:^{
+        XCTAssertEqual(1U, [self.coreDataManager fetchEntriesFromDataStore].count, @"");
+        
+        [self buildCoreDataManagerWithDefaultClient:NO];
+        [self.coreDataManager performSynchronizationWithSuccess:^{
+            XCTAssertEqual(2U, [self.coreDataManager fetchEntriesFromDataStore].count, @"");
+            
+            [self buildCoreDataManagerWithDefaultClient:NO];
+            [self.coreDataManager performSynchronizationWithSuccess:^{
+                XCTAssertEqual(1U, [self.coreDataManager fetchEntriesFromDataStore].count, @"");
+                
+                [self buildCoreDataManagerWithDefaultClient:NO];
+                [self.coreDataManager performSynchronizationWithSuccess:^{
+                    XCTAssertEqual(2U, [self.coreDataManager fetchEntriesFromDataStore].count, @"");
+                    
+                    [self buildCoreDataManagerWithDefaultClient:NO];
+                    EndBlock();
+                } failure:^(CDAResponse *response, NSError *error) {
+                    XCTFail(@"Error: %@", error);
+                    
+                    EndBlock();
+                }];
+            } failure:^(CDAResponse *response, NSError *error) {
+                XCTFail(@"Error: %@", error);
+                
+                EndBlock();
+            }];
+        } failure:^(CDAResponse *response, NSError *error) {
+            XCTFail(@"Error: %@", error);
+            
+            EndBlock();
+        }];
     } failure:^(CDAResponse *response, NSError *error) {
         XCTFail(@"Error: %@", error);
         
