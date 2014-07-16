ac7caca312b9ccd04dcb0ff1969b5e86c73c70f2
Boris Bügling <boris@icculus.org>
2014-07-16 15:45:40 +0200

    Accomodate changes in cocoapods-packager.

--- Examples/UFO/Distribution/update.sh
+++ Examples/UFO/Distribution/update.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
+VERSION=`pod spec cat ContentfulDeliveryAPI|grep version|head -n 1|cut -d\" -f4`
+
 rm -rf ContentfulDeliveryAPI.framework
-cp -Rp ../../../ContentfulDeliveryAPI-ios.framework ContentfulDeliveryAPI.framework
+cp -Rp ../../../ContentfulDeliveryAPI-$VERSION/ContentfulDeliveryAPI-ios.framework ContentfulDeliveryAPI.framework
