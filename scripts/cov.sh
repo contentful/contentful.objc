718791aab51a163f8562241ba9ee8ea044948820
Boris Bügling <boris@icculus.org>
2014-09-25 10:24:31 +0200

    Use cocoapods-coverage instead of homegrown stuff.

--- scripts/cov.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-
-INTERMEDIATES="`ls -d $HOME/Library/Developer/Xcode/DerivedData/ContentfulSDK-*/Build/Intermediates/ContentfulSDK.build/Debug-iphonesimulator/ContentfulDeliveryAPI.build`"
-killall CoverStory
-open `find "$INTERMEDIATES" -name '*.gcda'`
