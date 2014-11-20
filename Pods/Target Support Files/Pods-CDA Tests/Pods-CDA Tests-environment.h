0a8717d15e3722908b9e5007d562979ad453e23d
Boris Bügling <boris@icculus.org>
2014-11-20 14:02:44 +0100

    Update development Pods

--- Pods/Target Support Files/Pods-CDA Tests/Pods-CDA Tests-environment.h
+++ Pods/Target Support Files/Pods-CDA Tests/Pods-CDA Tests-environment.h
@@ -45,7 +45,7 @@
 // FBSnapshotTestCase
 #define COCOAPODS_POD_AVAILABLE_FBSnapshotTestCase
 #define COCOAPODS_VERSION_MAJOR_FBSnapshotTestCase 1
-#define COCOAPODS_VERSION_MINOR_FBSnapshotTestCase 1
+#define COCOAPODS_VERSION_MINOR_FBSnapshotTestCase 4
 #define COCOAPODS_VERSION_PATCH_FBSnapshotTestCase 0
 
 // OCMock
