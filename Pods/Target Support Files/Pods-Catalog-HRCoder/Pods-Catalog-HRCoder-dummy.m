01f6c559d07c0cae118263635947f68ee5ab6d42
Boris Bügling <boris@icculus.org>
2014-12-01 13:40:24 +0100

    Removed HRCoder depedency
    Performance can be quite bad compared to NSKeyedArchiver.

--- Pods/Target Support Files/Pods-Catalog-HRCoder/Pods-Catalog-HRCoder-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_Catalog_HRCoder : NSObject
-@end
-@implementation PodsDummy_Pods_Catalog_HRCoder
-@end
--- Pods/Target Support Files/Pods-CoreDataExample-HRCoder/Pods-CoreDataExample-HRCoder-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_CoreDataExample_HRCoder : NSObject
-@end
-@implementation PodsDummy_Pods_CoreDataExample_HRCoder
-@end
