d3a497f02f9b94b3de427c62612a3bdf34236c80
Boris Bügling <boris@icculus.org>
2015-08-31 10:22:14 -0400

    Ensure the script works on CP plugin load errors

--- Examples/upload.sh
+++ Examples/upload.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-VERSION=`pod spec cat ContentfulDeliveryAPI|grep version|head -n 1|cut -d\" -f4`
+VERSION=`pod spec cat ContentfulDeliveryAPI|grep version|tail -n 1|cut -d\" -f4`
 
 s3cmd --acl-public put UFO.zip s3://static.cdnorigin.contentful.com/downloads/iOS/UFO.zip
 
