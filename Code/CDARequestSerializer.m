5796d6c5e25e1ac81c1dea8a961eeed3285420aa
Boris Bügling <boris@icculus.org>
2016-02-10 13:41:50 +0100

    Bump version number

--- Code/CDARequestSerializer.m
+++ Code/CDARequestSerializer.m
@@ -18,7 +18,7 @@
     if (self) {
         [self setValue:[@"Bearer " stringByAppendingString:accessToken] forHTTPHeaderField:@"Authorization"];
 
-        self.userAgent = @"contentful.objc/1.9.4";
+        self.userAgent = @"contentful.objc/1.9.5";
     }
     return self;
 }
