1b7f1798dc5aae4196f3913cd843b51af3fc0750
JP Wright <jp@contentful.com>
2017-01-13 13:58:57 +0100

    Reimpleent UIKitAdditionsTests

---
+++ Tests/FakeNavigationController.h
@@ -0,0 +1,13 @@
+//
+//  FakeNavigationController.h
+//  ContentfulSDK
+//
+//  Created by JP Wright on 13/01/2017.
+//
+//
+
+#ifndef FakeNavigationController_h
+#define FakeNavigationController_h
+
+
+#endif /* FakeNavigationController_h */
---
+++ Tests/FakeNavigationController.m
@@ -0,0 +1,37 @@
+//
+//  FakeNavigationController.m
+//  ContentfulSDK
+//
+//  Created by JP Wright on 13/01/2017.
+//
+//
+
+#import "FakeNavigationController.h"
+
+@interface FakeNavigationController ()
+
+@end
+
+@implementation FakeNavigationController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
