a9c4800c75f82405b0eaf88dedbb4541ea450f9b
JP Wright <jp@contentful.com>
2017-05-02 15:29:23 +0200

    Generate docs with jazzy

--- scripts/reference-docs.sh
+++ scripts/reference-docs.sh
@@ -4,11 +4,7 @@ echo "Generating Jazzy Reference Documentation"
 
 SDK_VERSION=2.0.1
 SDK_NAME="ContentfulDeliveryAPI"
-<<<<<<< HEAD
 xcodebuild_arguments="--objc,ContentfulDeliveryAPI/ContentfulDeliveryAPI.h,--,-x,objective-c,-isysroot,$(xcrun --show-sdk-path),-I,$(pwd)"
-=======
-xcodebuild_arguments="--objc,Code/ContentfulDeliveryAPI.h,--,-x,objective-c,-isysroot,$(xcrun --show-sdk-path),-I,$(pwd)"
->>>>>>> New project structure to get jazzy to work
 
 jazzy \
   --clean \
@@ -19,13 +15,8 @@ jazzy \
   --github-file-prefix https://github.com/contentful/contentful.objc/tree/$SDK_VERSION \
   --module-version $SDK_VERSION \
   --module $SDK_NAME \
-<<<<<<< HEAD
   --umbrella-header ContentfulDeliveryAPI/ContentfulDeliveryAPI.h \
   --framework-root ./ContentfulDeliveryAPI/ \
-=======
-  --umbrella-header Code/ContentfulDeliveryAPI.h \
-  --framework-root ./Code/ \
->>>>>>> New project structure to get jazzy to work
   --sdk iphonesimulator \
   --theme apple
 #   --xcodebuild-arguments ${xcodebuild_arguments} \
