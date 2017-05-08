757e5c6a8846faea19ae95079228eb4704d8b6e3
JP Wright <jp@contentful.com>
2017-05-08 17:02:51 +0200

    Add make commands for CMA and add travis build phase

--- scripts/travis-build-test.sh
+++ scripts/travis-build-test.sh
@@ -4,13 +4,19 @@ set -x -o pipefail
 
 echo "Building"
 
-#make clean_simulators
 
-#rm -rf ${HOME}/Library/Developer/Xcode/DerivedData/*
 
+echo "Testing CDA SDK"
 # -jobs -- specify the number of concurrent jobs
 # `sysctl -n hw.ncpu` -- fetch number of 'logical' cores in macOS machine
 xcodebuild -jobs `sysctl -n hw.ncpu` test -workspace ContentfulSDK.xcworkspace -scheme ContentfulDeliveryAPI \
+  -sdk ${SDK} -destination "platform=iOS Simulator,name=${DEVICE_NAME},OS=${OS_VERSION}" \
+    ONLY_ACTIVE_ARCH=NO CODE_SIGNING_IDENTITY="" CODE_SIGNING_REQUIRED=NO | xcpretty -c
+
+
+echo "Testing CMA SDK"
+# Management SDK tests
+xcodebuild -jobs `sysctl -n hw.ncpu` test -workspace ContentfulSDK.xcworkspace -scheme ContentfulManagementAPI \
   -sdk ${SDK} -destination "platform=iOS Simulator,name=${DEVICE_NAME},OS=${OS_VERSION}" \
     ONLY_ACTIVE_ARCH=NO CODE_SIGNING_IDENTITY="" CODE_SIGNING_REQUIRED=NO | xcpretty -c
 
